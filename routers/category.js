88944cbfba25c7643e660af67d1049385b9b486c
dime <dime@danwi.me>
2015-11-08 16:30:03 +0800

    根据分类查询blog

--- routers/category.js
+++ routers/category.js
@@ -6,7 +6,10 @@
 var express = require("express");
 var router =  express.Router();
 
-router.get("/getCategory.do", function(request, response){
+/**
+ * 获取所有的目录
+ */
+router.get("/getCategory.do", function(request, response, next){
     console.log("success get getCategory.....");
     var MongoClient = require("mongodb").MongoClient;
     MongoClient.connect("mongodb://localhost:27017/myblog", function(err, db){
@@ -17,6 +20,32 @@ router.get("/getCategory.do", function(request, response){
                 if(!error){
                     console.log("success get collection ...")
                     collection.find({}).toArray(function(error, items){
+                        if(!error){
+                            console.log("success to find category....");
+                            console.log(items);
+                            response.send(items);
+                        }
+                    });
+                }
+            });
+        }
+    })
+});
+
+/**
+ * 根据目录获取所属博客分类
+ */
+router.post("getBlogByCategory.do", function(request, response, next){
+    var category = request.body.category;//拿到参数category
+    //查询mongo数据库数据
+    MongoClient.connect("mongodb://localhost:27017/myblog", function(err, db){
+        if(!err){
+            console.log("success connect mongoDB....");
+            //查询数据
+            db.collection("blog", function(error, collection){
+                if(!error){
+                    console.log("success get collection ...")
+                    collection.find({category:category}).toArray(function(error, items){
                         if(!error){
                             console.log("success to find category....");
                             console.log(items);
