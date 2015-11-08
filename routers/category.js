3f8e455c0caa60c5d69322204e4beb380d9d67c1
dime <dime@danwi.me>
2015-11-08 17:51:38 +0800

    根据category获取blog+++++++++++++

--- routers/category.js
+++ routers/category.js
@@ -38,7 +38,7 @@ router.get("/getCategory.do", function(request, response, next){
  */
 router.post("/getBlogByCategory.do", function(request, response, next){
     console.log("success getBlogByCategory.do");
-    //var categoryId = request.body.categoryId;//拿到参数category
+    var categoryId = request.body.categoryId;//拿到参数category
     //查询mongo数据库数据
     var MongoClient = require("mongodb").MongoClient;
     MongoClient.connect("mongodb://localhost:27017/myblog", function(err, db){
