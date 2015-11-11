3f998af17203bded7f11633ef013832c917aa236
dime <dime@danwi.me>
2015-11-11 23:57:43 +0800

    add view router

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 //静态页面路由
 app.use("/views",express.static(path.join(__dirname,"views")));
-app.use("/public", express.static(path.join(__dirname), "public"));
+app.use( express.static(path.join(__dirname), "public"));
 
 //request请求
 app.use("/category", category);
