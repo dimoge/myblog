034da7d967f9b420e6e745b9410c4f47b13c011e
dime <dime@danwi.me>
2015-11-12 00:02:45 +0800

    测试

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 //静态页面路由
 app.use("/views",express.static(path.join(__dirname,"views")));
+
 app.use(express.static(path.join(__dirname, 'public')));
 //request请求
 app.use("/category", category);
