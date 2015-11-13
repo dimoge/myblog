cd8e086522db9691815ce8a7bd2a8f40c09cad46
郭浩 <dime@danwi.me>
2015-11-13 12:17:10 +0800

    请求修复, 路由修改

--- routers/about.js
+++ routers/about.js
@@ -6,7 +6,7 @@ var router =  express.Router();
  * 获取我的信息
  **/
 
-router.get("getAboutMe.do", function(request, response){
+router.get("getBlogByCategory.do", function(request, response){
     console.log("router------/about/getAboutMe.do");
     var MongoClient = require("mongodb").MongoClient;
     MongoClient.connect("mongodb://localhost:27017/myblog", function(err, db){
