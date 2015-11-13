c08e2b438f53f4f6442efa71c47ec90f0735458e
郭浩 <dime@danwi.me>
2015-11-13 11:44:33 +0800

    添加about路由,添加ejs引擎的调用

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-//启动服务脚本
+//博客服务器启动脚本
 
-//外部模块
+//引入第三方model
 var express=require("express");
 var path=require("path");
 var bodyParser = require('body-parser');
-
-//自定义模块
 var app=express();
-var category=require("./routers/category.js");//category路由
 
-app.use(bodyParser.json());
+app.use(bodyParser.json());//使用bod-parser中间件解析request的数据
 app.use(bodyParser.urlencoded({extended: false}));
 app.set("view engine", "ejs");//使用ejs作为模板引擎
-//静态页面路由
+
+//自定义router
+var category=require("./routers/category.js");
+var about = require("./routers/about.js");
+//静态路由
 app.use("/views",express.static(path.join(__dirname,"views")));
-
 app.use("/public",express.static(path.join(__dirname, 'public')));
-//request请求
-app.use("/category", category);
 
+//request请求路由
+app.use("/category", category);
+app.use("/about", about);
+app.use();
 app.listen(3000);
 
 console.log("博客启动...../n 45.78.50.233:3000");
---
+++ routers/about.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var router =  express.Router();
+
+
+/**
+ * 获取我的信息
+ **/
+
+router.get("getAboutMe.do", function(request, response){
+    console.log("router------/about/getAboutMe.do");
+    var MongoClient = require("mongodb").MongoClient;
+    MongoClient.connect("mongodb://localhost:27017/myblog", function(err, db){
+        if(!err){
+            console.log("success connect mongoDB....");
+            //查询数据
+            db.collection("user", function(error, collection){
+                if(!error){
+                    console.log("success get collection ...")
+                    collection.find({}).toArray(function(error, items){
+                        if(!error){
+                            console.log("success to find category....");
+                            console.log(items);
+                            response.send(items);
+                            response.render("about", items);
+                            db.close();
+                        }
+                    });
+                }
+            });
+        }
+    })
+})
+
+module.exports = router;
