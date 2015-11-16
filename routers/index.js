65cb93f16f49fb76dd35d1036367f052a4ff2108
郭浩 <dime@danwi.me>
2015-11-16 14:40:26 +0800

    创建index路由++

--- routers/index.js
+++ routers/index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function(request, response){
+router.get("", function(request, response){
     response.render(index,{});
 });
 
