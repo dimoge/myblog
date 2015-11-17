c2cd637ce37b92c10b109b92f5d3e2b36aa97e5e
郭浩 <dime@danwi.me>
2015-11-17 16:02:39 +0800

    查询所有分类并显示

--- routers/admin/category.js
+++ routers/admin/category.js
@@ -8,7 +8,21 @@ var router = express.Router();
  * 查询所有分类
  */
 router.get("/getAll.do", function (request, response) {
-
+    var MongoClient = require("mongodb").MongoClient;
+    MongoClient.connect("mongodb://localhost:27017/myblog", function (err, db) {
+        if (!err) {
+            db.collection("category", function (error, collection) {
+                if (!error) {
+                    collection.find({}).toArray(function (error, items) {
+                        if (!error) {
+                            response.render("./admin/category", {"categories": items});
+                            db.close();
+                        }
+                    });
+                }
+            });
+        }
+    })
 });
 
 /**
